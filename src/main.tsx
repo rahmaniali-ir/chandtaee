3780629e7f05e1694f8c3fe88c27f0612564d288
Ali Rahmani <alirahmani.prog@gmail.com>
2025-11-03 17:22:51 +0330

    Update BrowserRouter basename for deployment configuration

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "./index.css"
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <DirectionProvider dir='rtl'>
-      <BrowserRouter>
+      <BrowserRouter basename='/chandtaee'>
         <App />
       </BrowserRouter>
     </DirectionProvider>
